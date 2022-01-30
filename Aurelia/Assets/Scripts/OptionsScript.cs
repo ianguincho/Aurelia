a7acaa418525367dd336f3b228ac1ab3b5ed4dc6
ianguincho <ian.guincho@gmail.com>
2022-01-30 01:19:48 -0500

    added resolution option
    uploading a working version of a resolution settings menu.

--- Aurelia/Assets/Scripts/OptionsScript.cs
+++ Aurelia/Assets/Scripts/OptionsScript.cs
@@ -2,10 +2,14 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.UI;
+using TMPro;
 
 public class OptionsScript : MonoBehaviour
 {
     public Toggle fullscreenToggle, vsyncToggle;
+    public List<ResItem> resolutions = new List<ResItem>();
+    private int selectedResolution;
+    public TMP_Text resolutionLabel;
 
     // Start is called before the first frame update
     void Start()
@@ -18,6 +22,29 @@ public class OptionsScript : MonoBehaviour
         }
         else
             vsyncToggle.isOn = true;
+
+        bool foundNativeRes = false;
+        for (int i = 0; i < resolutions.Count; i++)
+        {
+            if (Screen.width == resolutions[i].horizontal && Screen.height == resolutions[i].vertical)
+            {
+                foundNativeRes = true;
+                selectedResolution = i;
+                updateResLabel();
+            }
+        }
+
+        if(!foundNativeRes)
+        {
+            ResItem weirdRes = new ResItem();
+            weirdRes.horizontal = Screen.width;
+            weirdRes.vertical = Screen.height;
+
+            resolutions.Add(weirdRes);
+            selectedResolution = resolutions.Count - 1;
+            updateResLabel();
+        }
+
     }
 
     // Update is called once per frame
@@ -26,13 +53,49 @@ public class OptionsScript : MonoBehaviour
         
     }
 
+    public void resLeft()
+    {
+        if (selectedResolution <= 0)
+        {
+            selectedResolution = resolutions.Count - 1;
+        }
+        else
+            selectedResolution--;
+
+        updateResLabel();
+    }
+
+    public void resRight()
+    {
+        if (selectedResolution >= resolutions.Count - 1)
+        {
+            selectedResolution = 0;
+        }
+        else
+            selectedResolution++;
+
+        updateResLabel();
+    }
+
+    public void updateResLabel()
+    {
+        resolutionLabel.text = resolutions[selectedResolution].horizontal.ToString() + " x " + resolutions[selectedResolution].vertical.ToString();
+    }
+
     public void applyGraphics()
     {
-        Screen.fullScreen = fullscreenToggle.isOn;
 
         if (vsyncToggle.isOn)
             QualitySettings.vSyncCount = 1;
         else
             QualitySettings.vSyncCount = 0;
+
+        Screen.SetResolution(resolutions[selectedResolution].horizontal, resolutions[selectedResolution].vertical, fullscreenToggle.isOn);
     }
+}
+
+[System.Serializable]
+public class ResItem
+{
+    public int horizontal, vertical;
 }

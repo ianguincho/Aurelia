ba89743f62d7d2fe64a587f8dbf64ac12c11323d
YLiu1234 <yl18c@my.fsu.edu>
2022-02-01 23:30:39 -0500

    Added the Player Jump Code with the IsGrounded function

--- Aurelia/Assets/Scripts/Player.cs
+++ Aurelia/Assets/Scripts/Player.cs
@@ -3,8 +3,10 @@ using UnityEngine;
 public class Player : MonoBehaviour
 {
     [Header("Components")]
+    [SerializeField] private LayerMask platformsLayerMask;
     private Rigidbody2D playerRB;
     private Animator playerAnimator;
+    private BoxCollider2D playerCollider;
 
     [Header("Movement Variables")]
     [SerializeField] private float movementAcceleration = 70f;
@@ -15,13 +17,19 @@ public class Player : MonoBehaviour
     private bool changingDirection => (playerRB.velocity.x > 0f && horizontalDirection < 0f) || (playerRB.velocity.x < 0f && horizontalDirection > 0f);
 
     [Header("Jump Variables")]
-    [SerializeField] private float airLinearDrag = 2.5f;
+    [SerializeField] private float jumpForce = 20f;
+    [SerializeField] private float hangTime = .1f;
+    private float hangTimeCounter;
+    private float jumpBufferCounter;
+    bool isJumping = false;
+    private bool canJump => jumpBufferCounter > 0f && hangTimeCounter > 0f;
 
     // Start is called before the first frame update
     private void Start()
     {
         playerRB = GetComponent<Rigidbody2D>();
         playerAnimator = GetComponent<Animator>();
+        playerCollider = GetComponent<BoxCollider2D>();
     }
 
     // Update is called once per frame
@@ -29,9 +37,14 @@ public class Player : MonoBehaviour
     {
         horizontalDirection = getInput().x;
         verticalDirection = getInput().y;
+        //jump
+        if (isGrounded() && Input.GetKeyDown(KeyCode.Space))
+        {
+            playerRB.velocity = Vector2.up * jumpForce;            
+        }
+        
     }
-
-    private void FixedUpdate()
+       private void FixedUpdate()
     {
         playerRB.AddForce(new Vector2(horizontalDirection, 0f) * movementAcceleration);
         if (Mathf.Abs(playerRB.velocity.x) > maxMoveSpeed)
@@ -40,7 +53,7 @@ public class Player : MonoBehaviour
         if (Mathf.Abs(horizontalDirection) < 0.4f || changingDirection)
             playerRB.drag = groundLinearDrag;
         else
-            playerRB.drag = 0f;
+            playerRB.drag = 0f;      
     }
  
     private Vector2 getInput()
@@ -48,5 +61,9 @@ public class Player : MonoBehaviour
         return new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
 
-
+    private bool isGrounded()
+    {
+        RaycastHit2D raycastHit2d = Physics2D.BoxCast(playerCollider.bounds.center, playerCollider.bounds.size, 0f, Vector2.down, .1f, platformsLayerMask);
+        return raycastHit2d.collider != null;
+    }
 }

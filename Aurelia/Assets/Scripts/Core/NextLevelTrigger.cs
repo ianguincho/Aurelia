716113f62c0eb2df05834f78387c0749cd20e0db
YLiu1234 <yl18c@my.fsu.edu>
2022-03-17 19:11:19 -0400

    Added trigger code to go back between levels

--- Aurelia/Assets/Scripts/Core/NextLevelTrigger.cs
+++ Aurelia/Assets/Scripts/Core/NextLevelTrigger.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class EndTrigger : MonoBehaviour
+public class NextLevelTrigger : MonoBehaviour
 {
     private void OnTriggerEnter2D(Collider2D other)
     {
